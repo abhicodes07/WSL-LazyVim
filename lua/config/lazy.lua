f4185292bb83a7ea5585f08892c637f5cb6a1d0a
Abhinav <abhiasthana76@gmail.com>
2024-12-27 22:10:05 +0530

    Update: add ui border to lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
+  ui = { border = "double" }, -- add border to the window takes values: double, solid, rounded
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
