31874c306fd466b641d2778d8024fe2488db82dc
Abhinav <abhiasthana76@gmail.com>
2025-01-18 21:53:35 +0530

    add oxocarbon colorscheme

---
+++ lua/plugins/oxocarbon.lua
@@ -0,0 +1,10 @@
+return {
+  "nyoom-engineering/oxocarbon.nvim",
+  lazy = true,
+  priority = 1000,
+  styles = {
+    comments = { italic = true },
+    strings = { italic = true },
+    conditionals = { bold = true },
+  },
+}
