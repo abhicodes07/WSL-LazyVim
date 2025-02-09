c6c35508a82dbdf74bcb2008cc8008e2ff37f87e
Abhinav <abhiasthana76@gmail.com>
2025-02-09 14:41:47 +0530

    add lspkind for symbols like vscode

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -55,4 +55,76 @@ return {
       preset = "helix", -- Values : helix, classic, modern
     },
   },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+
+  -- lspkind : lsp icons
+  {
+    "onsails/lspkind.nvim",
+    event = "VeryLazy",
+    dependencies = { "hrsh7th/nvim-cmp" }, -- Ensure it's loaded after cmp
+    opts = function()
+      return {
+        mode = "symbol_text", -- Show both icons & text
+        preset = "codicons", -- Use VSCode icons
+        symbol_map = {
+          Text = "",
+          Method = "",
+          Function = "󰡱",
+          Constructor = "",
+          Field = "",
+          Variable = "",
+          Class = "",
+          Interface = "",
+          Module = "",
+          Property = "",
+          Unit = "",
+          Value = "",
+          Enum = "",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "",
+          Struct = "",
+          Event = "",
+          Operator = "",
+          TypeParameter = "",
+        },
+      }
+    end,
+  },
 }
