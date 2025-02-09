0716365a87a6d0e86eb2d480a0df0b80940ab8c0
Abhinav <abhiasthana76@gmail.com>
2025-02-09 14:39:03 +0530

    update

--- lua/plugins/oxocarbon.lua
+++ lua/plugins/oxocarbon.lua
@@ -2,9 +2,4 @@ return {
   "nyoom-engineering/oxocarbon.nvim",
   lazy = true,
   priority = 1000,
-  styles = {
-    comments = { italic = true },
-    strings = { italic = true },
-    conditionals = { bold = true },
-  },
 }
