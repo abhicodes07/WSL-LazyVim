c0ee4e64716836e2b51e4075b16b5be4077796fb
Abhinav <abhiasthana76@gmail.com>
2025-03-09 19:30:53 +0530

    add jinja treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = { "html", "python", "jinja", "htmldjango" }, -- Add jinja2
+    highlight = { enable = true },
+  },
+}
