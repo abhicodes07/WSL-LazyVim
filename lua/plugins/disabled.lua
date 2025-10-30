280a98356b865253753b7174297ffd346087132f
Abhinav <abhiasthana76@gmail.com>
2025-10-30 17:26:32 +0530

    update: revert updates

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  -- disable smear cursor
+  { "sphamba/smear-cursor.nvim", enabled = false },
+
+  -- disable colorizer
+  { "catgoose/nvim-colorizer.lua", enabled = false },
+}
--- lua/plugins/tailwind-css-lsp.lua
+++ lua/plugins/tailwind-css-lsp.lua
@@ -3,47 +3,18 @@ return {
   opts = {
     servers = {
       tailwindcss = {
-        root_dir = function(...)
-          return require("lspconfig.util").root_pattern(".git")(...)
-        end,
         -- exclude a filetype from the default_config
         filetypes_exclude = { "markdown" },
         -- add additional filetypes to the default_config
-        filetypes_include = {
-          "html",
-          "htmldjango",
-          "css",
-          "scss",
-          "javascript",
-          "javascriptreact",
-          "typescript",
-          "typescriptreact",
-          "svelte",
-          "vue",
-          "php",
-          "astro",
-          "templ",
-        },
+        filetypes_include = {},
         -- to fully override the default_config, change the below
         -- filetypes = {}
-      },
-    },
-    setup = {
-      tailwindcss = function(_, opts)
-        local tw = LazyVim.lsp.get_raw_config("tailwindcss")
-        opts.filetypes = opts.filetypes or {}
 
-        -- Add default filetypes
-        vim.list_extend(opts.filetypes, tw.default_config.filetypes)
-
-        -- Remove excluded filetypes
-        --- @param ft string
-        opts.filetypes = vim.tbl_filter(function(ft)
-          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-        end, opts.filetypes)
-
-        -- Additional settings for Phoenix projects
-        opts.settings = {
+        -- additional settings for the server, e.g:
+        -- tailwindCSS = { includeLanguages = { someLang = "html" } }
+        -- can be addeded to the settings table and will be merged with
+        -- this defaults for Phoenix projects
+        settings = {
           tailwindCSS = {
             includeLanguages = {
               elixir = "html-eex",
@@ -51,7 +22,21 @@ return {
               heex = "html-eex",
             },
           },
-        }
+        },
+      },
+    },
+    setup = {
+      tailwindcss = function(_, opts)
+        opts.filetypes = opts.filetypes or {}
+
+        -- Add default filetypes
+        vim.list_extend(opts.filetypes, vim.lsp.config.tailwindcss.filetypes)
+
+        -- Remove excluded filetypes
+        --- @param ft string
+        opts.filetypes = vim.tbl_filter(function(ft)
+          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+        end, opts.filetypes)
 
         -- Add additional filetypes
         vim.list_extend(opts.filetypes, opts.filetypes_include or {})
